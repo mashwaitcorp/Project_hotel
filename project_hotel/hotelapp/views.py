6678290959fa783083928cda2cb3d4fb71ceaf4b
Kabasele <pepexykabasele@gmail.com>
2021-07-24 15:30:30 +0200

    add the function  category hotel

--- project_hotel/hotelapp/views.py
+++ project_hotel/hotelapp/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
-from .models.hotel import Hotel
-from .forms.form import HotelForm
+from .models.hotel import Hotel, HotelCategory
+from .forms.form import HotelForm, HotelCategoryForm
 
 # Create your views here.
 
 
 class HotelCreate(CreateView):
-    """
-    Affichage du formulaire
-    """
+    
     model = Hotel
     form_class = HotelForm
     template_name="templates_hotel/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cat_form = HotelCategoryForm()
+        if cat_form.is_valid():
+            new_cat = cat_form.save(commit=False)
+            new_cat.save()
+
+        context["cat_form"] = cat_form
+        return context
+
+    
     
