f0575608598836c39d20f5d05bf3369f49c6ce28
Kabasele <pepexykabasele@gmail.com>
2021-07-28 14:13:56 +0200

    test the template

--- project_hotel/parametre/settings.py
+++ project_hotel/parametre/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,7 +126,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_ROOT = BASE_DIR / 'static'
+#STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = [
+
+    os.path.join(BASE_DIR, 'static'),
+
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
