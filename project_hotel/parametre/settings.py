45ff3d8bfb6698aaec337e28ae98197b20e26d22
Kabasele <pepexykabasele@gmail.com>
2021-07-28 14:22:59 +0200

    update settimgs file

--- project_hotel/parametre/settings.py
+++ project_hotel/parametre/settings.py
@@ -126,13 +126,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-#STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_DIRS = [
-
-    os.path.join(BASE_DIR, 'static'),
-
-    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
